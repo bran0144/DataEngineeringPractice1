85b575f8af440f42e34599cca1ea4ca4c1ad351a
KatieGott <Katie.Gott@target.com>
2021-10-22 15:07:21 -0500

    Streamlined data ingestion with pandas

---
+++ FlatFiles.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+tax_data = pd.read_csv("us_tax_data_2016.tsv", sep="\t")
+
+# Load TSV using the sep keyword argument to set delimiter
+data = pd.read_csv("vt_tax_data_2016.tsv", sep="\t")
+
+# Plot the total number of tax returns by income group
+counts = data.groupby("agi_stub").N1.sum()
+counts.plot.bar()
+plt.show()
+
+# Create list of columns to use
+cols = ['zipcode', 'agi_stub', 'mars1', 'MARS2', 'NUMDEP']
+
+# Create data frame from csv using only selected columns
+data = pd.read_csv("vt_tax_data_2016.csv", usecols=cols)
+
+# View counts of dependents and tax returns by income level
+print(data.groupby("agi_stub").sum())
+
+# Create data frame of next 500 rows with labeled columns
+vt_data_next500 = pd.read_csv("vt_tax_data_2016.csv", 
+                       		  nrows=500,
+                       		  skiprows=500,
+                       		  header=None,
+                       		  names=list(vt_data_first500))
+
+# View the Vermont data frames to confirm they're different
+print(vt_data_first500.head())
+print(vt_data_next500.head())
+
