ba8627a7e92402114671c55806ab30fc70aa9a88
KatieGott <Katie.Gott@target.com>
2021-10-25 18:37:00 -0500

    working with magic commands and line_profiler

--- EfficientCode.py
+++ EfficientCode.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 # Using range, enumerate, map, and list comprehensions to write more efficient code
 # Create a range object that goes from 0 to 5
 nums = range(6)
@@ -46,3 +48,47 @@ print(names_uppercase)
 # nums2_np[:,0]
 # nums_np > 0
 # nums_np[nums_np > 0]
+
+# Print second row of nums
+print(nums[1,:])
+
+# Print all elements of nums that are greater than six
+print(nums[nums > 6])
+
+# Double every element of nums
+nums_dbl = nums * 2
+print(nums_dbl)
+
+# Replace the third column of nums
+nums[0:,2] = nums[0:,2] + 1
+print(nums)
+
+# Create a list of arrival times
+arrival_times = [*range(10,60,10)]
+
+# Convert arrival_times to an array and update the times
+arrival_times_np = np.array(arrival_times)
+new_times = arrival_times_np - 3
+
+# Use list comprehension and enumerate to pair guests to new times
+guest_arrivals = [(names[i],time) for i,time in enumerate(new_times)]
+
+# Map the welcome_guest function to each (guest,time) pair
+welcome_map = map(welcome_guest, guest_arrivals)
+
+guest_welcomes = [*welcome_map]
+print(*guest_welcomes, sep='\n')
+
+# IPython - magic commands %
+# %lsmagic lists all magic commands
+# %timeit -r2 -n10 rand_nums = np.random.rand(1000)
+# -r (# of runs) -n (number of loops)
+# (double %%) %timeit cell magic mode lets you run multiple lines of code
+# times = %timeit -o rand_nums = np.random.rand(1000) (saves it into an output)
+
+# Code profiling
+# detailed stats on frequency and duration of function calls (summary statistics)
+# line_profiler (need to pip install)
+# %load_ext line_profiler
+# %lprun -f convert_units convert_units(heroes, hts, wts)
+
