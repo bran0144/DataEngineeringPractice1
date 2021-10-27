2171c98f0360d3c3a3b562ef0e67df7a7acfc842
KatieGott <Katie.Gott@target.com>
2021-10-26 20:36:51 -0500

    more efficient looping

--- EfficientCode.py
+++ EfficientCode.py
@@ -92,3 +92,176 @@ print(*guest_welcomes, sep='\n')
 # %load_ext line_profiler
 # %lprun -f convert_units convert_units(heroes, hts, wts)
 
+# Combining objects with zip
+# combined_zip=zip(names, hps)
+# returns a zip object that must be unpacked into a list
+# combined_zip_list = [*combined_zip]
+# Collections module
+# namedtuple - tuple subclasses with named fields
+# deque - list like container with fast appends and pops
+# Counter - dict for counting hashable objects
+# OrderedDict - dictionary that retains order of entries
+# defaultDict - dict that calls a factory function to supply missing values
+# from collections import Counter
+# type_counts = Counter(poke_types) poke_types is a list
+# returns a dictionary with the counts of each item from list
+# itertools module
+# itertools.combinations()
+# combos_obj= cominations(poke_types, 2) - returns a combinations object
+# combos = [*combos_obj]
+
+# Combine all three lists together
+names_types = [*zip(names, primary_types, secondary_types)]
+
+print(*names_types[:5], sep='\n')
+
+# Combine five items from names and three items from primary_types
+differing_lengths = [*zip(names[:5], primary_types[:3])]
+
+print(*differing_lengths, sep='\n')
+
+# Collect the count of primary types
+type_count = Counter(primary_types)
+print(type_count, '\n')
+
+# Collect the count of generations
+gen_count = Counter(generations)
+print(gen_count, '\n')
+
+# Use list comprehension to get each Pokémon's starting letter
+starting_letters = [name[0] for name in names]
+
+# Collect the count of Pokémon for each starting_letter
+starting_letters_count = Counter(starting_letters)
+print(starting_letters_count)
+
+# Import combinations from itertools
+from itertools import combinations
+
+# Create a combination object with pairs of Pokémon
+combos_obj = combinations(pokemon, 2)
+print(type(combos_obj), '\n')
+
+# Convert combos_obj to a list by unpacking
+combos_2 = [*combos_obj]
+print(combos_2, '\n')
+
+# Collect all possible combinations of 4 Pokémon directly into a list
+combos_4 = [*combinations(pokemon, 4)]
+print(combos_4)
+
+# Set theory
+# intersection() - all elements that are in both sets
+# difference() - all elements in one but not the other
+# symmetric_difference() - all elements in exactly one set
+# union() - all elements that are in either set
+# membership testing (check if a value exists) - use the in operator
+# convert each list into a set, then use .intersection() method
+# set_a.intersection(set_b)
+# Membership testing in a set is much faster than a tuple or list
+# all items must be unique in sets
+
+# Convert both lists to sets
+ash_set = set(ash_pokedex)
+misty_set = set(misty_pokedex)
+
+# Find the Pokémon that exist in both sets
+both = ash_set.intersection(misty_set)
+print(both)
+
+# Find the Pokémon that Ash has and Misty does not have
+ash_only = ash_set.difference(misty_set)
+print(ash_only)
+
+# Find the Pokémon that are in only one set (not both)
+unique_to_set = ash_set.symmetric_difference(misty_set)
+print(unique_to_set)
+
+# Convert Brock's Pokédex to a set
+brock_pokedex_set = set(brock_pokedex)
+print(brock_pokedex_set)
+
+# Check if Psyduck is in Ash's list and Brock's set
+print('Psyduck' in ash_pokedex)
+print('Psyduck' in brock_pokedex_set)
+
+# Check if Machop is in Ash's list and Brock's set
+print('Machop' in ash_pokedex)
+print('Machop' in brock_pokedex_set)
+
+# Use find_unique_items() to collect unique Pokémon names
+uniq_names_func = find_unique_items(names)
+print(len(uniq_names_func))
+
+# Convert the names list to a set to collect unique Pokémon names
+uniq_names_set = set(names)
+print(len(uniq_names_set))
+
+# Check that both unique collections are equivalent
+print(sorted(uniq_names_func) == sorted(uniq_names_set))
+
+# Use the best approach to collect unique primary types and generations
+uniq_types = set(primary_types) 
+uniq_gens = set(generations)
+print(uniq_types, uniq_gens, sep='\n') 
+
+# Eliminating loops
+# flat is better than nested
+# using list comprehension: totals_comp = [sum(row) for row in poke_stats]
+# using map: totals_map = [*map(sum, poke_stats)]
+# numpy array instead of a list of lists
+# avgs_np = poke_stats.mean(axis=1)
+
+# Collect Pokémon that belong to generation 1 or generation 2
+gen1_gen2_pokemon = [name for name,gen in zip(poke_names, poke_gens) if gen < 3]
+
+# Create a map object that stores the name lengths
+name_lengths_map = map(len, gen1_gen2_pokemon)
+
+# Combine gen1_gen2_pokemon and name_lengths_map into a list
+gen1_gen2_name_lengths = [*zip(gen1_gen2_pokemon, name_lengths_map)]
+
+print(gen1_gen2_name_lengths_loop[:5])
+print(gen1_gen2_name_lengths[:5])
+
+# Create a total stats array
+total_stats_np = stats.sum(axis=1)
+
+# Create an average stats array
+avg_stats_np = stats.mean(axis=1)
+
+# Combine names, total_stats_np, and avg_stats_np into a list
+poke_list_np = [*zip(names, total_stats_np, avg_stats_np)]
+
+print(poke_list_np == poke_list, '\n')
+print(poke_list_np[:3])
+print(poke_list[:3], '\n')
+top_3 = sorted(poke_list_np, key=lambda x: x[1], reverse=True)[:3]
+print('3 strongest Pokémon:\n{}'.format(top_3))
+
+# Collect all possible pairs using combinations()
+possible_pairs = [*combinations(pokemon_types, 2)]
+
+# Create an empty list called enumerated_tuples
+enumerated_tuples = []
+
+# Append each enumerated_pair_tuple to the empty list above
+for i,pair in enumerate(possible_pairs, 1):
+    enumerated_pair_tuple = (i,) + pair
+    enumerated_tuples.append(enumerated_pair_tuple)
+
+# Convert all tuples in enumerated_tuples to a list
+enumerated_pairs = [*map(list, enumerated_tuples)]
+print(enumerated_pairs)
+
+# Calculate the total HP avg and total HP standard deviation
+hp_avg = hps.mean()
+hp_std = hps.std()
+
+# Use NumPy to eliminate the previous for loop
+z_scores = (hps - hp_avg)/hp_std
+
+# Combine names, hps, and z_scores
+poke_zscores2 = [*zip(names, hps, z_scores)]
+print(*poke_zscores2[:3], sep='\n')
+
