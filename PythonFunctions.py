2cd9fc5035f2a29d114488c915ff7ac4b89421c8
KatieGott <Katie.Gott@target.com>
2021-10-29 10:44:40 -0500

    completed Writing Functions in Python

--- PythonFunctions.py
+++ PythonFunctions.py
@@ -485,3 +485,60 @@ print(hello('Alice'))
 #       return wrapper
 #   return decorator
 
+def tag(*tags):
+  # Define a new decorator, named "decorator", to return
+  def decorator(func):
+    # Ensure the decorated function keeps its metadata
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+      # Call the function being decorated and return the result
+      return func(*args, **kwargs)
+    wrapper.tags = tags
+    return wrapper
+  # Return the new decorator
+  return decorator
+
+def returns(return_type):
+  # Complete the returns() decorator
+  def decorator(func):
+    def wrapper(*args, **kwargs):
+      result = AssertionError
+      assert type(result) == return_type
+      return result
+    return wrapper
+  return decorator
+  
+@returns(dict)
+def foo(value):
+  return value
+
+try:
+  print(foo([1,2,3]))
+except AssertionError:
+  print('foo() did not return a dict!')
+
+@tag('test', 'this is a tag')
+def foo():
+  pass
+
+print(foo.tags)
+
+def returns_dict(func):
+  # Complete the returns_dict() decorator
+  def wrapper(*args, **kwargs):
+    result = AssertionError
+    assert type(result) == dict
+    return result
+  return wrapper
+  
+@returns_dict
+def foo(value):
+  return value
+
+try:
+  print(foo([1,2,3]))
+except AssertionError:
+  print('foo() did not return a dict!')
+
+
+
