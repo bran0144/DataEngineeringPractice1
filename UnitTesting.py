e1bfa086a848cf877e907e91b16d226c859f237c
KatieGott <Katie.Gott@target.com>
2021-11-16 15:09:28 -0600

    completed all modules

--- UnitTesting.py
+++ UnitTesting.py
@@ -296,3 +296,252 @@ class TestGetDataAsNumpyArray(object):
 # install codecov app in github
 # get the badge from setting in the gui (cut and paste the markdown to the Readme file)
 
+# Using setup and teardown for testing
+def test_on_raw_data():
+    raw_path, clean_path = raw_and_clean_datafile
+    preprocess(raw_data_file_path, clean_data_file_path)
+    with open(clean_data_file_path) as f:
+        lines = f.readlines()
+    first_line = lines[0]
+    assert first_line == "1801\t201411\n"
+    second_line = lines[1]
+    assert second_line == "2002\t333209\n"
+    # teardown so that next test gets a clean environment
+
+# In pytest, the setup and teardown happens outside the test in a function called fixture
+@pytest.fixture
+def my_fixure():
+    # Do setup here
+    yield data  
+    # do teardown
+
+# test accesses the data from fixture by calling fixtured passed as an arg
+def test_something(my_fixure):
+    ...
+    data = my_fixure
+    ...
+import os
+# Setup for fixture for test_on_raw_data:
+@pytest.fixure
+def raw_and_clean_data_file():
+    raw_data_file_path = "raw.txt"
+    clean_data_file_path = "clean.txt"
+    with open (raw_data_file_path, "w") as f:
+        f.write("1,801\t201,411\n"
+        "1,767565,122\n"
+        "2,002\t33,209\n"
+        "1990\t782,911\n"
+        "1,285\t389129\n")
+    yield raw_data_file_path, clean_data_file_path
+    os.remove(raw_data_file_path)
+    os.remove(clean_data_file_path)
+
+# tmpdir fixture - builtin
+# creates a temp dir during setup and deletes during teardown
+# Fixture chaining: setup for tmpdir, setup of raw_and_clean_data_file(), test, teardown of file, teardown of tmpdir
+@pytest.fixture
+def raw_and_clean_data_file(tmpdir):
+   raw_date_file_path = tmpdir.join("raw.txt")
+   clean_data_file_path = tmpdir.join("clean.txt")
+    #rest of the code stays the same except you don't need to do the os.remove anymore
+
+# Exercises:
+# Add a decorator to make this function a fixture
+@pytest.fixture
+def clean_data_file():
+    file_path = "clean_data_file.txt"
+    with open(file_path, "w") as f:
+        f.write("201\t305671\n7892\t298140\n501\t738293\n")
+    yield file_path
+    os.remove(file_path)
+    
+# Pass the correct argument so that the test can use the fixture
+def test_on_clean_file(clean_data_file):
+    expected = np.array([[201.0, 305671.0], [7892.0, 298140.0], [501.0, 738293.0]])
+    # Pass the clean data file path yielded by the fixture as the first argument
+    actual = get_data_as_numpy_array(clean_data_file, 2)
+    assert actual == pytest.approx(expected), "Expected: {0}, Actual: {1}".format(expected, actual) 
+
+@pytest.fixture
+def empty_file():
+    # Assign the file path "empty.txt" to the variable
+    file_path = "empty.txt"
+    open(file_path, "w").close()
+    # Yield the variable file_path
+    yield file_path
+    # Remove the file in the teardown
+    os.remove(file_path)
+    
+def test_on_empty_file(self, empty_file):
+    expected = np.empty((0, 2))
+    actual = get_data_as_numpy_array(empty_file, 2)
+    assert actual == pytest.approx(expected), "Expected: {0}, Actual: {1}".format(expected, actual)
+
+# Mocking
+# Need pytest-mock and unittest.mock (python standard lib)
+# mocker.patch("data.prepocessing_helpers.row_to_list")
+# returns the MagicMock object which we store in the variable row_to_list_mock
+def row_to_list_bug_free(row):
+# just creates a dictionary of the inputs and the correct outputs (does not run the function)
+    return_values = {...}
+    return return_values[row]
+# this means that correct values will always be returned
+from unittest.mock import call
+def test_on_raw_data(raw_and_clean_data_file, mocker,):
+    raw_path, clean_path = raw_and_clean_data_file
+    row_to_list_mock = mocker.patch("data.preprocessing_helpers.row_to_list")
+    row_to_list_mock.side_effect = row_to_list_bug_free
+
+def test_on_raw_data(raw_and_clean_data_file, mocker,):
+    raw_path, clean_path = raw_and_clean_data_file
+    row_to_list_mock = mocker.patch("data.preprocessing_helpers.row_to_list", side_effect = row_to_list_bug_free)
+    preprocess(raw_path, clean_path)
+    assert row_to_list_mock.call_args_list == [
+        call("1,801\t201,411\n"),
+        call("1,767565,112\n")
+    ]
+
+row_to_list_mock.call_args_list     #attribute is a list of all the arguments that row_to_list was called with
+#args_list is wrapped in a convenience object called cal() that needs to be imported from unittest.mock
+
+# Exercises:
+
+# Define a function convert_to_int_bug_free
+def convert_to_int_bug_free(comma_separated_integer_string):
+    # Assign to the dictionary holding the correct return values 
+    return_values = {"1,801": 1801, "201,411": 201411, "2,002": 2002, "333,209": 333209, "1990": None, "782,911": 782911, "1,285": 1285, "389129": None}
+    # Return the correct result using the dictionary return_values
+    return return_values[comma_separated_integer_string]
+
+# Add the correct argument to use the mocking fixture in this test
+def test_on_raw_data(self, raw_and_clean_data_file, mocker):
+    raw_path, clean_path = raw_and_clean_data_file
+    # Replace the dependency with the bug-free mock
+    convert_to_int_mock = mocker.patch("data.preprocessing_helpers.convert_to_int",
+                                       side_effect=convert_to_int_bug_free)
+    preprocess(raw_path, clean_path)
+    # Check if preprocess() called the dependency correctly
+    assert convert_to_int_mock.call_args_list == [call("1,801"), call("201,411"), call("2,002"), call("333,209"), call("1990"), call("782,911"), call("1,285"), call("389129")]
+    with open(clean_path, "r") as f:
+        lines = f.readlines()
+    first_line = lines[0]
+    assert first_line == "1801\\t201411\\n"
+    second_line = lines[1]
+    assert second_line == "2002\\t333209\\n" 
+
+# Testing Models
+
+from data.preprocessing_helpers import preprocess
+from features.as_numpy import get_data_as_numpy_array
+from models.train import split_into_training_and_testing_sets
+
+preprocess("data/raw/housing_data.txt", "data/clean/clean_housing_data.txt")
+data = get_data_as_numpy_array("data/clean/clean_housing_data.txt", 2)
+training_set, testing_set = split_into_training_and_testing_sets(data)
+slope, intercept = train_model(training_set)
+
+
+# Linear Regression Model
+from scipy.stats import linregress
+
+def train_model(training_set):
+    slope, intercept, _,_,_ = linregress(training_set[:, 0], training_set[:,1])
+    return slope, intercept
+
+# Hard to test when you don't know the return value (like in a regression model)
+# So, one trick is to use a simple dataset where the return value is known
+# 
+def test_on_linear_data():
+    test_argument = np.array([[1.0, 3.0], [2.0, 5.0]])
+    expected_slope = 2.0
+    expected_intercept = 1.0
+    slope, intercept = train_model(test_argument)
+    assert slope == pytest.approx(expected_slope)
+    assert intercept == pytest.approx(expected_intercept)
+
+# Or we can use inequalities
+# If we can't predict the best fit line, but we know the best fit line is positive we can assert it is greater than 0
+
+def test_on_positively_correlated_data():
+    test_argument = np.array([[1.0, 4.0], [2.0, 4.0], [3.0, 9.0]])
+    slope, intercept = train_model(test_argument)
+    assert slope > 0
+
+# Just because models are hard to test, doesn't mean we don't need to
+# Try to perform sanity checks
+
+import numpy as np
+import pytest
+from models.train import model_test
+
+def test_on_perfect_fit():
+    # Assign to a NumPy array containing a linear testing set
+    test_argument = np.array([[1.0, 3.0], [2.0, 5.0], [3.0, 7.0]])
+    # Fill in with the expected value of r^2 in the case of perfect fit
+    expected = 1.0
+    # Fill in with the slope and intercept of the model
+    actual = model_test(test_argument, slope=2.0, intercept=1.0)
+    # Complete the assert statement
+    assert actual == pytest.approx(expected), "Expected: {0}, Actual: {1}".format(expected, actual)
+
+def test_on_circular_data(self):
+    theta = pi/4.0
+    # Assign to a NumPy array holding the circular testing data
+    test_argument = np.array([[1.0, 0.0], [cos(theta), sin(theta)],
+                              [0.0, 1.0],
+                              [cos(3 * theta), sin(3 * theta)],
+                              [-1.0, 0.0],
+                              [cos(5 * theta), sin(5 * theta)],
+                              [0.0, -1.0],
+                              [cos(7 * theta), sin(7 * theta)]]
+                             )
+    # Fill in with the slope and intercept of the straight line
+    actual = model_test(test_argument, slope=0.0, intercept=0.0)
+    # Complete the assert statement
+    assert actual == pytest.approx(0.0)
+
+#Testing Plots
+# Function to test:
+#  def get_plot_for_best_fit_line(slope, intercept, x_array, y_array, title):
+# returns a matplotlib.figure.Figure() Object
+# Has lots of properties including configuration and style, so not advisable to test each individually
+# Instead we use one-time baseline generation:
+# -decide on test arguments
+# -call plotting function on test arguments
+# -convert Figure() into PNG image
+# -inspect image manually, verify, store as baseline image
+# Testing step:
+# -call plotting funciton on test arguments
+# -convert Figure() to PNG image
+# -compare image with stored baseline image
+#  Need to use pytest-mpl for image comparisons
+# ignores OS related differences
+# instead of assert statement, it returns 
+# return get_plot_for_best_fit_line(slope, intercept, x_array, y_array, title)
+# Need to add @pytest.mark.mpl_image_compare decorator
+# this will do the image comparison and baseline image generation under the hood
+# baseline images need to be stored in separate directory in tests/visualization
+# to run test:
+# pytest -k "test_plot_for_linear_data" --mpl-generate-path visualization/baseline
+# next time we run the test we must use the --mpl option to make pyests compare with baseline image
+# pytest -k "test_plot_for_linear_data" --mpl
+# IF the test fails, both images and the differences by pixel will be saved in a temp folder
+
+# Exercises:
+from visualization.plots import get_plot_for_best_fit_line
+
+class TestGetPlotForBestFitLine(object):
+    # Add the pytest marker which generates baselines and compares images
+    @pytest.mark.mpl_image_compare
+    def test_plot_for_almost_linear_data(self):
+        slope = 5.0
+        intercept = -2.0
+        x_array = np.array([1.0, 2.0, 3.0])
+        y_array = np.array([3.0, 8.0, 11.0])
+        title = "Test plot for almost linear data"
+        # Return the matplotlib figure returned by the function under test
+        return get_plot_for_best_fit_line(slope, intercept, x_array, y_array, title)
+
+# command:
+# pytest --mpl-generate-path /home/repl/workspace/project/tests/visualization/baseline -k "test_plot_for_almost_linear_data"
+
