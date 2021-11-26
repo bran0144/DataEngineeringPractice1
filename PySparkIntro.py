b55d06060f1e1fa4b640a74a3280c8f28e48c332
KatieGott <Katie.Gott@target.com>
2021-11-26 09:22:57 -0600

    completed 2nd and 3rd modules

--- PySparkIntro.py
+++ PySparkIntro.py
@@ -87,3 +87,196 @@ flights.show()
 # Add duration_hrs
 flights = flights.withColumn("duration_hrs", flights.air_time /60) 
 
+# Spark and SQL similarities
+# .filter() - similar to WHERE clause
+
+# Both of these work:
+# flights.filter("air_time > 120").show()
+# flights.filter(flights.air_time > 120).show()
+
+# .filter can take a string
+# Or a column of boolean values
+
+# Filter flights by passing a string
+long_flights1 = flights.filter("distance > 1000")
+
+# Filter flights by passing a column of boolean values
+long_flights2 = flights.filter(flights.distance > 1000)
+
+# Print the data to check they're equal
+long_flights1.show()
+long_flights2.show()
+
+# .select() is like sql SELECT
+# Arguments can be column name as a string or a column object (df.col)
+# When you pass a column object, you can do math operations (like add and subtract)
+# .select() will return only the columns you want, where .withColumn() returns all the columns (so it is heavier weight). Better to use .select() to not move data you don’t need
+
+# Select the first set of columns
+selected1 = flights.select("tailnum", "origin", "dest")
+
+# Select the second set of columns
+temp = flights.select(flights.origin, flights.dest, flights.carrier)
+
+# Define first filter
+filterA = flights.origin == "SEA"
+
+# Define second filter
+filterB = flights.dest == "PDX"
+
+# Filter the data, first by filterA then by filterB
+selected2 = temp.filter(filterA).filter(filterB)
+
+# .alias() method allows you to rename the column you’re selecting
+
+# flights.select((flights.air_time/60).alias(“duration_hrs"))
+
+# .selectExpr() - lets you use sql expressions as a string
+# flights.selectExpr("air_time/60 as duration_hrs”)
+
+# Define avg_speed
+avg_speed = (flights.distance/(flights.air_time/60)).alias("avg_speed")
+
+# Select the correct columns
+speed1 = flights.select("origin", "dest", "tailnum", avg_speed)
+
+# Create the same table using a SQL expression
+speed2 = flights.selectExpr("origin", "dest", "tailnum", "distance/(air_time/60) as avg_speed”)
+GroupedData methods (.min(), .max(), .count())
+
+# df.groupBy().min(“col”).show()
+# created an object, to use the min method, find the min value in col and returns it as a DF
+
+# Find the shortest flight from PDX in terms of distance
+flights.filter(flights.origin == "PDX").groupBy().min("distance").show()
+
+# Find the longest flight from SEA in terms of air time
+flights.filter(flights.origin == “SEA").groupBy().max("air_time").show()
+
+# Average duration of Delta flights
+flights.filter(flights.carrier == "DL").filter(flights.origin == "SEA").groupBy().avg("air_time").show()
+
+# Total hours in the air
+flights.withColumn("duration_hrs", flights.air_time/60).groupBy().sum(“duration_hrs").show()
+
+# Grouped dataFrames: 
+# pyspark.sql.GroupedData
+
+# Can pass one or more columns to the .groupBy() , just like in SQL
+
+# Group by tailnum
+by_plane = flights.groupBy("tailnum")
+
+# Number of flights each plane made
+by_plane.count().show()
+
+# Group by origin
+by_origin = flights.groupBy("origin")
+
+# Average duration of flights from PDX and SEA
+by_origin.avg(“air_time").show()
+
+# .agg() method - can pass an aggregate column expression that uses functions from pyspark.sql.functions
+
+# Import pyspark.sql.functions as F
+import pyspark.sql.functions as F
+
+# Group by month and dest
+by_month_dest = flights.groupBy("month", "dest")
+
+# Average departure delay by month and destination
+by_month_dest.avg("dep_delay").show()
+
+# Standard deviation of departure delay
+by_month_dest.agg(F.stddev(“dep_delay")).show()
+
+# Joins
+# .join() - takes three arguments
+# - second df that you want to join
+# - on - name of key column as a string (names of key column must be same in each table
+# - how - specifies the kind of join - common is “leftouter”  
+
+# Examine the data
+print(airports.show())
+
+# Rename the faa column
+airports = airports.withColumnRenamed("faa", "dest")
+
+# Join the DataFrames
+flights_with_airports = flights.join(airports, on="dest", how="leftouter")
+
+# Examine the new DataFrame
+print(flights_with_airports.show())
+
+# Machine learning pipelines
+# pyspark.ml library - two main classes- Transformer, Estimator
+# Transformer - transform() - takes a df and returns a new df (usually the original with a new column) - other common classes are Bucketizer and PCA
+# Estimator - all implement the fit() method. Takes a df and returns a model object (like StringIndexerModel or RandomForestsModel)
+
+# Rename year column
+planes = planes.withColumnRenamed("year", "plane_year")
+
+# Join the DataFrames
+model_data = flights.join(planes, on="tailnum", how="leftouter")
+
+# Spark only handles numeric data, so types must be ints or floats (called doubles in spark)
+# To tell spark how to import datatypes: 
+# .cast() method with the withColumn() method
+# .withColumn(cast(“integer”))
+
+# Cast the columns to integers
+model_data = model_data.withColumn("arr_delay", model_data.arr_delay.cast("integer"))
+model_data = model_data.withColumn("air_time", model_data.air_time.cast("integer"))
+model_data = model_data.withColumn("month", model_data.month.cast("integer"))
+model_data = model_data.withColumn("plane_year", model_data.plane_year.cast(“integer"))
+
+# Create the column plane_age
+model_data = model_data.withColumn("plane_age", model_data.year - model_data.plane_year)
+
+# Create is_late
+model_data = model_data.withColumn("is_late", model_data.arr_delay > 0)
+
+# Convert to an integer
+model_data = model_data.withColumn("label", model_data.is_late.cast("integer"))
+
+# Remove missing values
+model_data = model_data.filter("arr_delay is not NULL and dep_delay is not NULL and air_time is not NULL and plane_year is not NULL”)
+
+# How to deal with strings, when spark wants numeric data?
+# pyspark.ml.features submodule
+# Can create ‘one-hot vectors’
+# Steps:
+# - create a StringIndexer to encode categorical data (Members of this class are Estimators that take a DataFrame with a column of strings and map each unique string to a number. Then, the Estimator returns a Transformer that takes a DataFrame, attaches the mapping to it as metadata, and returns a new DataFrame with a numeric column corresponding to the string column.
+# - use OneHotEncoder - creates an Estimator, then a Transformer (encodes your feature as a vector for ml models
+
+# Create a StringIndexer
+carr_indexer = StringIndexer(inputCol="carrier", outputCol="carrier_index")
+
+# Create a OneHotEncoder
+carr_encoder = OneHotEncoder(inputCol="carrier_index", outputCol=“carrier_fact")
+
+# Create a StringIndexer
+dest_indexer = StringIndexer(inputCol="dest", outputCol="dest_index")
+
+# Create a OneHotEncoder
+dest_encoder = OneHotEncoder(inputCol="dest_index", outputCol=“dest_fact")
+
+# Last step in the pipeline is to combine all the columns containing features into a single column for ml
+# pyspark.ml-feature has a VectorAssembler class for this
+
+# Make a VectorAssembler
+vec_assembler = VectorAssembler(inputCols=["month", "air_time", "carrier_fact", "dest_fact", "plane_age"], outputCol=“features")
+
+# Import Pipeline
+from pyspark.ml import Pipeline
+
+# Make the pipeline
+flights_pipe = Pipeline(stages=[dest_indexer, dest_encoder, carr_indexer, carr_encoder, vec_assembler])
+
+# Make sure to only split data into test/train after all of the transformations
+
+# Fit and transform the data
+piped_data = flights_pipe.fit(model_data).transform(model_data)
+
+# Split the data into training and test sets
+training, test = piped_data.randomSplit([.6, .4])
